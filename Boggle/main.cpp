73dbdf1dde210d60869e4e2a8dd9f9c01e449b88
Caspar Wylie <casparwylie@caspars-mbp.home>
2016-08-01 11:44:48 +0100

    Initial Commit

---
+++ Boggle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Boggle
+//
+//  Created by Caspar Wylie on 01/08/2016.
+//  Copyright © 2016 Caspar Wylie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
